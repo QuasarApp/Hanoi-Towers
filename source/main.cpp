26d0b83cc6c57fe9298e232947003cea93a767f7
EndrII <EndrIIMail@gmail.com>
2018-05-13 23:11:35 +0300

    statick font

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,29 @@
 #include <QApplication>
+#include <QFont>
+#include <QFontDatabase>
 //#include <QScreen>
 #include <QQmlApplicationEngine>
 #include <QQmlComponent>
 #include "backEnd.h"
 #include <QTranslator>
+
+
+void setFont(const QApplication& app){
+
+    QString fontPath = "://ubuntu";
+    int fontId = QFontDatabase::addApplicationFont(fontPath);
+    if (fontId != -1)
+    {
+        QFont font(QFontDatabase::applicationFontFamilies(fontId).at(0));
+        app.setFont(font);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    setFont(app);
+
     QTranslator translator;
 
     QString defaultLocale = QLocale::system().name();
