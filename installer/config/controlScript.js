2aad0d70a4b0b68c69d31826ed619d0fdb801882
EndrII <EndrIIMail@gmail.com>
2018-09-09 13:09:50 +0300

    fix run programm on windows

--- installer/config/controlScript.js
+++ installer/config/controlScript.js
@@ -2,17 +2,22 @@ function Controller()
 {
     installer.uninstallationFinished.connect(this, Controller.prototype.uninstallationFinished);
     installer.installationFinished.connect(this, Controller.prototype.installationFinished);
+    runProgramControl();
 }
 
 
-Controller.prototype.uninstallationFinished = function()
-{
+function runProgramControl() {
+    if (systemInfo.kernelType === "winnt") {
+        Controller.setValue("RunProgram", qsTr("HanoiTowers.exe"));
+    }
+}
+
+Controller.prototype.uninstallationFinished = function() {
 
 }
 
 
-Controller.prototype.installationFinished = function()
-{
+Controller.prototype.installationFinished = function() {
 
 }
 
