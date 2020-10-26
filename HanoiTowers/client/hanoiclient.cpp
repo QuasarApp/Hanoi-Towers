86a732cdf99b6f649f84de13fe3d2a70552c6b88
EndrII <EndrIIMail@gmail.com>
2020-10-26 15:21:46 +0300

    simple update

--- HanoiTowers/client/hanoiclient.cpp
+++ HanoiTowers/client/hanoiclient.cpp
@@ -7,19 +7,49 @@
 
 #include "hanoiclient.h"
 #include <qmlnotifyservice.h>
+#include <user.h>
+#include <userdata.h>
+#include <sqldbcache.h>
 
 HanoiClient::HanoiClient() {
-    initSqlDb();
+    initSqlDb("", new QH::SqlDBCache(DEFAULT_UPDATE_INTERVAL, QH::SqlDBCasheWriteMode::Force));
 
 }
 
 QH::ParserResult HanoiClient::parsePackage(const QH::Package &pkg,
                                            const QH::AbstractNodeInfo *sender) {
+    auto parentResult = DataBaseNode::parsePackage(pkg, sender);
+    if (parentResult != QH::ParserResult::NotProcessed) {
+        return parentResult;
+    }
 
+    if (H_16<UserData>() == pkg.hdr.command) {
+        UserData obj(pkg);
+
+        QH::BaseId requesterId = getSender(sender, &obj);
+
+        if (setObject(requesterId, &obj) != QH::DBOperationResult::Allowed) {
+            return QH::ParserResult::Error;
+        }
+
+        return QH::ParserResult::Processed;
+
+    } else if (H_16<QH::PKG::User>() == pkg.hdr.command) {
+        QH::PKG::User obj(pkg);
+
+        if (db()->saveObject(&obj)) {
+            return QH::ParserResult::Error;
+        }
+
+    }
+
+    return QH::ParserResult::NotProcessed;
 }
 
 QStringList HanoiClient::SQLSources() const {
+//    return {
 
+//    };
 }
 
 void HanoiClient::handleError(const QString &error) {
--- HanoiTowers/client/hanoiclient.h
+++ HanoiTowers/client/hanoiclient.h
@@ -30,6 +30,9 @@ public:
 
 private slots:
     void handleError(const QString& error);
+
+
+
 };
 
 #endif // HANOICLIENT_H
