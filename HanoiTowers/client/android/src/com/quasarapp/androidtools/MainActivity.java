63f872a8b8ad9783645aafdf330313b5c46a61be
EndrII <EndrIIMail@gmail.com>
2021-05-07 10:13:53 +0300

    simple fix activyty class

--- HanoiTowers/client/android/src/com/quasarapp/androidtools/MainActivity.java
+++ HanoiTowers/client/android/src/com/quasarapp/androidtools/MainActivity.java
@@ -5,7 +5,7 @@
  * of this license document, but changing it is not allowed.
 */
 
-package com/quasarapp/androidtools;
+package com.quasarapp.androidtools;
 
 import org.qtproject.qt5.android.bindings.QtActivity;
 import android.view.View;
