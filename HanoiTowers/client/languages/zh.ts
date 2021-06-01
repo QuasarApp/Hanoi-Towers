aa7a282d7d725b25df5fc7b39c30c2a010602b3b
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2021-06-01 15:59:46 +0800

    l10n(zh): add human translation
    Since the English translation is generated by a program, not by a
      English-speaking person, the meaning of the sentence is verbose and/or
      unclear. I have to rewrite the Chinese translation according to my own
      understanding, so there may be a lot of modifications.

--- HanoiTowers/client/languages/zh.ts
+++ HanoiTowers/client/languages/zh.ts
@@ -13,7 +13,7 @@
     </message>
     <message>
         <source>Credits</source>
-        <translation>学分</translation>
+        <translation>鸣谢</translation>
     </message>
     <message>
         <source>Product of QuasarApp
@@ -24,21 +24,21 @@
             * Contact: https://github.com/EndrII
             * Copyright (C) 2018-2019 Yankovich N. Andrei.
             * Copyright (C) 2019-2021 QuasarApp group.</source>
-        <translation>QuasarApp产品
-             * 开发商：
-             * 程序员：Yankovich N. Andrei。
-             * 设计者：Chernenkov I. Oleg。
+        <translation>QuasarApp 的产品
+             * 开发者：
+             * 程序员：Yankovich N. Andrei
+             * 设计师：Chernenkov I. Oleg
              * 本游戏在 LGPLv3 许可下发行。
              * 联系方式：https://github.com/EndrII
-             * 版权所有 (C) 2018-2019 Yankovich N. Andrei。
-             * 版权所有 (C) 2019-2021 QuasarApp 组。</translation>
+             * 版权所有 (C) 2018-2019 Yankovich N. Andrei 。
+             * 版权所有 (C) 2019-2021 QuasarApp group 。</translation>
     </message>
 </context>
 <context>
     <name>Config</name>
     <message>
         <source>Standart color</source>
-        <translation>标准色</translation>
+        <translation>标准颜色</translation>
     </message>
     <message>
         <source>Random color</source>
@@ -46,46 +46,46 @@
     </message>
     <message>
         <source>Animation</source>
-        <translation>动画片</translation>
+        <translation>动画</translation>
     </message>
     <message>
         <source>Fog</source>
-        <translation>多雾路段</translation>
+        <translation>雾气</translation>
     </message>
     <message>
         <source>Fog Animations</source>
-        <translation>雾动画</translation>
+        <translation>雾气动画</translation>
     </message>
 </context>
 <context>
     <name>Game</name>
     <message>
         <source>Congratulations! You have passed the level %0</source>
-        <translation>恭喜！ 您已通过级别 %0</translation>
+        <translation>恭喜！您通过了第 %0 级</translation>
     </message>
     <message>
         <source>You got &lt;b&gt;%0&lt;/b&gt; points for this level and unlocked the level &lt;b&gt;%1&lt;/b&gt;!</source>
-        <translation>您获得了此级别的 &lt;b&gt;%0&lt;/b&gt; 分并解锁了 &lt;b&gt;%1&lt;/b&gt; 级！</translation>
+        <translation>您在本级获得了 &lt;b&gt;%0&lt;/b&gt; 分并解锁了第 &lt;b&gt;%1&lt;/b&gt; 级！</translation>
     </message>
     <message>
         <source>&lt;br&gt;&lt;br&gt; Totally &lt;b&gt;%2&lt;/b&gt; steps.</source>
-        <translation>&lt;br&gt;&lt;br&gt; 总共 &lt;b&gt;%2&lt;/b&gt; 个步骤。</translation>
+        <translation>&lt;br&gt;&lt;br&gt; 一共用了 &lt;b&gt;%2&lt;/b&gt; 步。</translation>
     </message>
     <message>
         <source>&lt;br&gt; The minimum number of steps for this level : &lt;b&gt;%3&lt;/b&gt;</source>
-        <translation>&lt;br&gt; 此级别的最小步数：&lt;b&gt;%3&lt;/b&gt;</translation>
+        <translation>&lt;br&gt; 本级最少可用 &lt;b&gt;%3&lt;/b&gt; 步</translation>
     </message>
     <message>
         <source>You got &lt;b&gt;%0&lt;/b&gt; points for this level!</source>
-        <translation>您在此级别获得了 &lt;b&gt;%0&lt;/b&gt; 分！</translation>
+        <translation>您在本级获得了 &lt;b&gt;%0&lt;/b&gt; 分！</translation>
     </message>
     <message>
         <source>&lt;br&gt;&lt;br&gt; Totally &lt;b&gt;%1&lt;/b&gt; steps.</source>
-        <translation>&lt;br&gt;&lt;br&gt; 总共 &lt;b&gt;%1&lt;/b&gt; 个步骤。</translation>
+        <translation>&lt;br&gt;&lt;br&gt; 一共用了 &lt;b&gt;%1&lt;/b&gt; 步。</translation>
     </message>
     <message>
         <source>&lt;br&gt; The minimum number of steps for this level : &lt;b&gt;%2&lt;/b&gt;</source>
-        <translation>&lt;br&gt; 此级别的最小步数：&lt;b&gt;%2&lt;/b&gt;</translation>
+        <translation>&lt;br&gt; 本级最少可用 &lt;b&gt;%2&lt;/b&gt; 步</translation>
     </message>
 </context>
 <context>
@@ -96,7 +96,7 @@
     </message>
     <message>
         <source>step </source>
-        <translation>步 </translation>
+        <translation>步数 </translation>
     </message>
 </context>
 <context>
@@ -118,7 +118,7 @@
     <name>HanoiTowers</name>
     <message>
         <source>Create</source>
-        <translation>创造</translation>
+        <translation>创建</translation>
     </message>
     <message>
         <source>Connect error</source>
@@ -126,19 +126,19 @@
     </message>
     <message>
         <source>Failed to connect to server please check network connection befor login</source>
-        <translation>无法连接到服务器请在登录前检查网络连接</translation>
+        <translation>无法连接到服务器，请检查网络连接</translation>
     </message>
     <message>
         <source>Update Profile error</source>
-        <translation>更新配置文件错误</translation>
+        <translation>更新用户资料错误</translation>
     </message>
     <message>
         <source>Failed to update yuo user data please check network connection befor update prifile</source>
-        <translation>未能更新您的用户数据，请在更新配置文件前检查网络连接</translation>
+        <translation>未能更新您的用户数据，请检查网络连接</translation>
     </message>
     <message>
         <source>Update Profile</source>
-        <translation>更新个人信息</translation>
+        <translation>更新个人资料</translation>
     </message>
     <message>
         <source>User data updated successful</source>
@@ -154,11 +154,11 @@
     </message>
     <message>
         <source>Register online error</source>
-        <translation>注册在线错误</translation>
+        <translation>在线注册错误</translation>
     </message>
     <message>
         <source>Failed to register this account, if this account was created by you, try to restore it.</source>
-        <translation>注册此帐号失败，如果此帐号是您创建的，请尝试恢复它。</translation>
+        <translation>帐号注册失败，如果此帐号曾经是您创建的，请尝试恢复它。</translation>
     </message>
     <message>
         <source>Server error</source>
@@ -166,11 +166,11 @@
     </message>
     <message>
         <source>User with this id is not registered. If it you then use please the sigup form.</source>
-        <translation>具有该ID的用户未注册。 如果您使用，请填写注册表单。</translation>
+        <translation>此 ID 未被注册。 如果您想使用它，请进行注册。</translation>
     </message>
     <message>
         <source>User with this id already registered. If it you then use please the login form.</source>
-        <translation>具有此 ID 的用户已注册。 如果它然后您使用请登录表单。</translation>
+        <translation>此 ID 已被注册。 如果您拥有这个 ID ，请进行登录。</translation>
     </message>
     <message>
         <source>Create user error</source>
@@ -182,7 +182,7 @@
     </message>
     <message>
         <source>Remove online error</source>
-        <translation>删除在线错误</translation>
+        <translation>在线删除错误</translation>
     </message>
     <message>
         <source>current profile not online!</source>
@@ -201,35 +201,35 @@
     <name>Help</name>
     <message>
         <source>Tap to select the upper part of tower.</source>
-        <translation>点击以选择塔的上部。</translation>
+        <translation>选择塔顶部的方块。</translation>
     </message>
     <message>
         <source>Tap to move the selected part to another column.</source>
-        <translation>点击以将所选部分移动到另一列。</translation>
+        <translation>将所选方块移动到另一列。</translation>
     </message>
     <message>
         <source>The playing field consists of 3 columns, your task is to move all parts of the tower from the first column to the third.</source>
-        <translation>比赛场地由3列组成，您的任务是将塔的所有部分从第一列移到第三列。</translation>
+        <translation>游戏场地中共有 3 列，您的任务是将塔的所有方块从第一列移动到第三列。</translation>
     </message>
     <message>
         <source>Remember you can&apos;t put bigger part on smaller.</source>
-        <translation>请记住，您不能将较大的部分放在较小的部分。</translation>
+        <translation>记住，较小的方块上不能放置较大的方块。</translation>
     </message>
     <message>
         <source>You will move smaller objects on larger ones in such a way.</source>
-        <translation>您将以这种方式在较大的物体上移动较小的物体。</translation>
+        <translation>较大的方块上则能够放置较小的方块。</translation>
     </message>
     <message>
         <source>Try to make as few moves as possible. Good luck!</source>
-        <translation>尽量少移动。 祝你好运！</translation>
+        <translation>试着将移动的次数降至最少。 祝您好运！</translation>
     </message>
     <message>
         <source>Tap here if you want to change tower high.</source>
-        <translation>如果您想更改塔高，请点击此处。</translation>
+        <translation>点击此处调整塔高。</translation>
     </message>
     <message>
         <source>Tap here if you want to return to main menu.</source>
-        <translation>如果您想返回主菜单，请点击此处。</translation>
+        <translation>点击此处返回主菜单。</translation>
     </message>
     <message>
         <source>Do not show again</source>
@@ -248,7 +248,7 @@
     </message>
     <message>
         <source>HanoiTowers World</source>
-        <translation>河内塔世界</translation>
+        <translation>汉诺塔世界</translation>
     </message>
     <message>
         <source>About</source>
@@ -256,7 +256,7 @@
     </message>
     <message>
         <source>Config</source>
-        <translation>设定档</translation>
+        <translation>设置</translation>
     </message>
     <message>
         <source>Users</source>
@@ -275,26 +275,26 @@
     </message>
     <message>
         <source>Especially for you, we have a premium version without ads. Nothing else will stop you from completing the levels!</source>
-        <translation>特别是对于您，我们有一个没有广告的高级版本。 没有什么能阻止你完成关卡！</translation>
+        <translation>我们为您特别提供了一个不带广告的高级版本。 没有什么能阻止您完成关卡！</translation>
     </message>
     <message>
         <source>Get Premium Hanoi towers</source>
-        <translation>获取高级河内塔</translation>
+        <translation>获取汉诺塔高级版</translation>
     </message>
 </context>
 <context>
     <name>PremiumThanks</name>
     <message>
         <source>Thanks for your support!</source>
-        <translation>谢谢你的支持！</translation>
+        <translation>感谢您的支持！</translation>
     </message>
     <message>
         <source>You are now our premium user and we won&apos;t dare bother you with ads anymore.</source>
-        <translation>您现在是我们的高级用户，我们再也不敢用广告打扰您了。</translation>
+        <translation>您现在是我们的高级用户，不会再被广告打扰了。</translation>
     </message>
     <message>
         <source>Do not show again </source>
-        <translation>不再显示</translation>
+        <translation>不再显示 </translation>
     </message>
 </context>
 <context>
@@ -312,15 +312,15 @@
     </message>
     <message>
         <source>Welcom to Hanoi Towers </source>
-        <translation>欢迎来到河内塔</translation>
+        <translation>欢迎来到汉诺塔 </translation>
     </message>
     <message>
         <source>Profiles</source>
-        <translation>简介</translation>
+        <translation>用户资料</translation>
     </message>
     <message>
         <source>This are the main settings</source>
-        <translation>这是主要设置</translation>
+        <translation>主要设置</translation>
     </message>
     <message>
         <source>About Page</source>
@@ -328,14 +328,14 @@
     </message>
     <message>
         <source>World Of Hanoi Towers</source>
-        <translation>河内塔世界</translation>
+        <translation>汉诺塔世界</translation>
     </message>
 </context>
 <context>
     <name>UserView</name>
     <message>
         <source>Please choose a new Avatar</source>
-        <translation>请选择一个新的头像</translation>
+        <translation>请选择一个新头像</translation>
     </message>
     <message>
         <source>Id</source>
@@ -355,11 +355,11 @@
     </message>
     <message>
         <source>Remove</source>
-        <translation>去掉</translation>
+        <translation>删除</translation>
     </message>
     <message>
         <source>Restore</source>
-        <translation>恢复</translation>
+        <translation>还原</translation>
     </message>
     <message>
         <source>Accept</source>
@@ -367,7 +367,7 @@
     </message>
     <message>
         <source>Remove %0 user</source>
-        <translation>删除 %0 用户</translation>
+        <translation>删除用户 %0</translation>
     </message>
     <message>
         <source>All saved data and records will be delete, Do you want continuee?</source>
@@ -378,7 +378,7 @@
     <name>UsersTable</name>
     <message>
         <source>Locale users list</source>
-        <translation>语言环境用户列表</translation>
+        <translation>本地用户列表</translation>
     </message>
     <message>
         <source>Add user</source>
@@ -393,7 +393,7 @@
     <name>main</name>
     <message>
         <source>Hanoi Towers</source>
-        <translation>河内塔</translation>
+        <translation>汉诺塔</translation>
     </message>
 </context>
 </TS>
